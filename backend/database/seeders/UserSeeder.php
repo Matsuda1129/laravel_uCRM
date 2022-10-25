64e69a1f43d61e843a18c2c1fa03c9d9eb31b953
Takuya Matsuda <matsuda@ank3.jp>
2022-10-25 20:57:56 +0900

    items preparation

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password123')
+        ]);
+    }
+}
